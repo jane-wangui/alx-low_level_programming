fbdfb4b4adfe424e251b36027da4fe19cec6aac6
jane-wangui <janewangui.se@gmail.com>
2022-12-12 10:27:11 -0800

    fixing betty

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,13 +9,13 @@ int main(void)
 	unsigned char a = '0';
 	int b;
 
-	for ( b = 0; b < 10; b++)
+	for (b = 0; b < 10; b++)
 	{
 		putchar(a);
 		a++;
 	}
 	a = '1';
-	for ( b = 0; b < 6; b++)
+	for (b = 0; b < 6; b++)
 	{
 		putchar('0' + a);
 		a++;
