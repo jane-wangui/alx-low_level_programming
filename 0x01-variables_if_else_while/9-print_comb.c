29b2b98fbaf021abdf3ab9806ce26854e919d465
jane-wangui <janewangui.se@gmail.com>
2022-12-12 09:41:04 -0800

    combination

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+
+int j;
+int i;
+	
+	for ( i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			putchar(i + '0');
+			putchar(j + '0');
+			putchar(',');
+		}
+	}
+putchar('\n');
+return (0);
+}
