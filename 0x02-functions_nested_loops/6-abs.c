2592c02706b0c182d80a6fc3d8748954390350e6
jane-wangui <janewangui.se@gmail.com>
2022-12-13 14:14:39 -0800

    absolute int value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs - compute the absolute value of an int
+ *
+ * @n: Int to make absolute
+ *
+ * Return: Int ot int tims negative
+ */
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
