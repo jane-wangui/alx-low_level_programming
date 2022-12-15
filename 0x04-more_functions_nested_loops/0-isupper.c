3431d3e073dc34644d81b42bbcdb1ab0c022bc94
jane-wangui <janewangui.se@gmail.com>
2022-12-15 15:41:41 -0800

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character
+ *
+ * @c: character to be checked
+ *
+ * Return: return 1 if uppercase or 0 if lowercase
+ */
+int _isupper(int c)
+{
+	char c;
+
+	if ( c = "A" && c <= "Z")
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}	
