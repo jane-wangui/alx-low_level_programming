65d98db58a2be4f4995a2e6f3f0aa0f6ac6befdd
jane-wangui <janewangui.se@gmail.com>
2022-12-15 16:32:54 -0800

    print 1-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_numbers - print numbers 1 to 9
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int x = 0;
+
+	while (x <= 9)
+	{
+		_putchar(x);
+	}
+	_putchar('\n');
+	return (0);
+}
