0a8be30942ac55358828c4a6d40cf6735c59b4b2
jane-wangui <janewangui.se@gmail.com>
2022-12-15 17:02:58 -0800

    print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_most_numbers - print all numbers 1 to 9 except 2 and 4
+ *
+ * Return: Void
+ */
+void print_most_numbers(void)
+{
+	char x;
+
+	for (x = '0'; x <= '9'; x++)
+	{
+		if (x != "2" && x != "4")
+		{
+			_putchar(x);
+		}
+	}
+	-putchar('\n');
+}
