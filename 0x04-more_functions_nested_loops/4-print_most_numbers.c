9acbed3064cdc9ee20d7622edfe308501af85588
jane-wangui <janewangui.se@gmail.com>
2022-12-15 17:11:42 -0800

    print more numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,10 +10,10 @@ void print_most_numbers(void)
 
 	for (x = '0'; x <= '9'; x++)
 	{
-		if (x != "2" && x != "4")
+		if (x != '2' && x != '4')
 		{
 			_putchar(x);
 		}
 	}
-	-putchar('\n');
+	_putchar('\n');
 }
