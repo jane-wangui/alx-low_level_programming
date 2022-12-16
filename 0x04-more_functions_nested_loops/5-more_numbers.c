3a67b1e3ef2eb32e07bb0ef12c14aef8a0237f96
jane-wangui <janewangui.se@gmail.com>
2022-12-15 17:45:11 -0800

    print

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,19 @@
  */
 void more_numbers(void)
 {
-	int a;
-	int b;
+int a;
+int b;
 
-	for (a = 1; a <= 10; a++)
-	{
-		for (b = 0; b <= 14; b++)
-		{
-			if (b >= 10)
-				_putchar('1');
-			_putchar(b % 10 + '0');
-		}
-		_putchar('\n);
-	}
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 14; b++)
+{
+if (b > 9)
+{
+_putchar(b / 10 + '0');
+}
+_putchar(b % 10 + '0');
+}
+_putchar('\n);
+}
 }
