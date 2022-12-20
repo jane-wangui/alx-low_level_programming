a902f74cb2b1c2d189b6ea4990168ccd5599bb6a
jane-wangui <janewangui.se@gmail.com>
2022-12-19 16:41:40 -0800

    print string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_rev - print the reverse of a string
+ * @s: string to print
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	int n;
+
+	n = 0;
+
+	while (s[n] != '\0')
+		n++;
+	for (i = n - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
