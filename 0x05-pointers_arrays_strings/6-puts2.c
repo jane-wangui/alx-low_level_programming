927a61a0dec8bfcf82ba5532c06ab6abf4831d79
jane-wangui <janewangui.se@gmail.com>
2022-12-19 17:26:18 -0800

    prit evry other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * puts2 - print every other character
+ * @str: character to print
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
