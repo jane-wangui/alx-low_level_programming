0ba56d67dda8e19a2c1fab8a0ea3194708f389b3
jane-wangui <janewangui.se@gmail.com>
2022-12-29 11:04:32 -0800

    fixing betty warning

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,14 +11,13 @@ char *_strncat(char *dest, char *src, int n)
 	int i;
 	int j;
 
-	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; dest[j] != '\0'; j++)
 	{
-		continue;
 	}
-	for (j = 0; src[j] != '\0'; && j < n; j++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[i +j] = src[j];
+		dest[j + i] = src[i];
 	}
-	dest[i +j] = '\0';
+	dest[j + i] = '\0';
 	return (dest);
 }
