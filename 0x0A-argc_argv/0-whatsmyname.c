c74bba59f4639251519440e8975865ba32255b0f
jane-wangui <janewangui.se@gmail.com>
2023-01-14 13:55:47 -0800

    argv 0 what is my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program to print the progrma name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	argc = argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
