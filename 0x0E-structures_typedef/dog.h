a4de67555e6082f4d3f1814ab457214f3d5e0bda
jane-wangui <janewangui.se@gmail.com>
2023-01-16 15:40:20 -0800

    my first struct

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+/**
+  * struct dog - structure for dog
+  * @name: name of the dog
+  * @age: age of the dog
+  * @owner: name of the owner
+* Description: A struct to hold values for a dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
