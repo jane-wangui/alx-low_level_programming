c5f06121524d3590f33f4e24fa3d9babb24190a9
jane-wangui <janewangui.se@gmail.com>
2023-07-28 17:46:20 -0700

    file change

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,6 @@
-#ifndef MAIN_H
-#define MAIN_H_
+#ifndef LISTS_H
+#define LISTS_H
+
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -23,3 +24,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
